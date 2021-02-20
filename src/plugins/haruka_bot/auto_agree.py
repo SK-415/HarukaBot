02dc2d94d604adfad719a6c0637ab25f7d4996d4
SK-415 <2967923486@qq.com>
2021-02-21 00:59:35 +0800

    use current event type for auto_agree

--- src/plugins/haruka_bot/auto_agree.py
+++ src/plugins/haruka_bot/auto_agree.py
@@ -1,5 +1,5 @@
 from nonebot import on_request
-from nonebot.adapters.cqhttp import Bot, Event
+from nonebot.adapters.cqhttp import Bot, FriendRequestEvent, GroupRequestEvent
 from nonebot.permission import SUPERUSER
 from nonebot.adapters.cqhttp.permission import PRIVATE_FRIEND
 
@@ -8,14 +8,14 @@ from nonebot.adapters.cqhttp.permission import PRIVATE_FRIEND
 friend_req = on_request(priority=5)
 
 @friend_req.handle()
-async def _(bot: Bot, event: Event, state: dict):
-    if event.request_type == 'friend' and str(event.user_id) in bot.config.superusers:
+async def _(bot: Bot, event: FriendRequestEvent, state: dict):
+    if str(event.user_id) in bot.config.superusers:
         await bot.set_friend_add_request(flag=event.flag, approve=True)
 
 
 group_invite = on_request(priority=5)
 
 @group_invite.handle()
-async def _(bot: Bot, event: Event, state: dict):
-    if event.request_type == 'group' and event.sub_type == 'invite' and str(event.user_id) in bot.config.superusers:
+async def _(bot: Bot, event: GroupRequestEvent, state: dict):
+    if event.sub_type == 'invite' and str(event.user_id) in bot.config.superusers:
         await bot.set_group_add_request(flag=event.flag, sub_type='invite', approve=True)
