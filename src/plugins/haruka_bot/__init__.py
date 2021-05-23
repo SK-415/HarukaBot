419f8666b449f7a323d2c8f92f2c557d55cd373c
SK-415 <2967923486@qq.com>
2021-05-24 00:10:26 +0800

    rename config_manage.py to commands.py

--- src/plugins/haruka_bot/__init__.py
+++ src/plugins/haruka_bot/__init__.py
@@ -3,7 +3,7 @@ import nonebot
 try:
     nonebot.get_driver()
     from . import utils
-    from . import config_manager
+    from . import commands
     from . import live_pusher
     from . import dynamic_pusher
     from . import auto_agree
