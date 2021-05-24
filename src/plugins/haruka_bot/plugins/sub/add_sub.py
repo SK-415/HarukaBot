6dbe78134d4e92c9871a0b8584c0dfc6808e6682
SK-415 <2967923486@qq.com>
2021-05-25 00:51:03 +0800

    refactor add_sub

--- src/plugins/haruka_bot/plugins/sub/add_sub.py
+++ src/plugins/haruka_bot/plugins/sub/add_sub.py
@@ -3,11 +3,13 @@ from nonebot.adapters.cqhttp import Bot
 from nonebot.adapters.cqhttp.event import MessageEvent
 from nonebot.typing import T_State
 
-from ...database import Config
+from ...database import DB
 from ...utils import permission_check, to_me
+from ...libs.bilireq import BiliReq, RequestError
 
 
 add_sub = on_command('关注', aliases={'添加主播',}, rule=to_me(), priority=5)
+add_sub.__doc__ = """关注 UID"""
 
 add_sub.handle()(permission_check)
 
@@ -17,8 +19,28 @@ async def get_args(bot: Bot, event: MessageEvent, state: T_State):
     if args:
         state['uid'] = args
 
-@add_sub.got('uid', prompt='请输入主播的 uid')
+@add_sub.got('uid', prompt='请输入要关注的UID')
 async def _(bot: Bot, event: MessageEvent, state: T_State):
+    """根据 UID 订阅 UP 主"""
+
     uid = state['uid']
-    with Config(event) as config:
-        await add_sub.finish(await config.add_uid(uid))
+    async with DB() as db:
+        user = await db.get_user(uid)
+        name = user and user.name
+    if not name:
+        br = BiliReq()
+        try:
+            name = (await br.get_info(uid))['name']
+        except RequestError as e:
+            if e.code == -400 or e.code == -404:
+                await add_sub.finish("UID不存在，注意UID不是房间号")
+            elif e.code == -412:
+                await add_sub.finish("操作过于频繁IP暂时被风控，请半小时后再尝试")
+            else:
+                await add_sub.finish(f"未知错误，请联系开发者反馈，错误内容：\n\
+                                    {str(e)}")
+    async with DB() as db:
+        result = await db.add_sub_by_event(uid, name, event)
+    if result:
+        await add_sub.finish(f"已关注 {name}（{uid}）")
+    await add_sub.finish(f"请勿重复关注 {name}（{uid}）")
