e53e101fcf254f346ebd608e3eacf97530557f11
SK-415 <2967923486@qq.com>
2021-05-30 04:42:18 +0800

    update load method

--- src/plugins/haruka_bot/__init__.py
+++ src/plugins/haruka_bot/__init__.py
@@ -3,14 +3,10 @@ from nonebot.plugin.manager import PluginLoader
 
 from .config import Config
 
-try:
+
+if isinstance(globals()['__loader__'], PluginLoader):
     global_config = get_driver().config
     config = Config(**global_config.dict())
-    if isinstance(globals()['__loader__'], PluginLoader):
-        from . import plugins
-    else:
-        from . import database
-except ValueError:
-    pass
+    from . import plugins
 
 from .version import VERSION, __version__
