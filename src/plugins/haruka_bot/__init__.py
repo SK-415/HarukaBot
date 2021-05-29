b570d1cb3fe8e804693ae28000d37e08995dcdca
SK-415 <2967923486@qq.com>
2021-05-29 19:00:42 +0800

    update __init__.py

--- src/plugins/haruka_bot/__init__.py
+++ src/plugins/haruka_bot/__init__.py
@@ -1,11 +1,15 @@
 from nonebot import get_driver
+from nonebot.plugin.manager import PluginLoader
 
 from .config import Config
 
 try:
     global_config = get_driver().config
     config = Config(**global_config.dict())
-    from . import plugins
+    if isinstance(globals()['__loader__'], PluginLoader):
+        from . import plugins
+    else:
+        from . import database
 except ValueError:
     pass
 
