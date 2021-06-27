b6923014828db6d2086c1ce0dbe3e4e79cb4f389
SK-415 <2967923486@qq.com>
2021-06-27 23:43:53 +0800

    add delete_pyppeteer

--- src/plugins/haruka_bot/config.py
+++ src/plugins/haruka_bot/config.py
@@ -1,12 +1,13 @@
-# 注：其他文件中出现的类似 from .. import config，均是从 __init__.py 导入的 Config 实例
 from typing import Optional
 from pydantic import BaseSettings
 
 
+# 其他地方出现的类似 from .. import config，均是从 __init__.py 导入的 Config 实例
 class Config(BaseSettings):
     
     haruka_dir: Optional[str] = None
     haruka_to_me: bool = True
+    haruka_delete_pyppeteer: bool = False
 
     class Config:
         extra = "ignore"
--- src/plugins/haruka_bot/utils/browser.py
+++ src/plugins/haruka_bot/utils/browser.py
@@ -1,9 +1,14 @@
 import base64
+import shutil
+from pathlib import Path
 from typing import Optional
 
+from appdirs import AppDirs
 from nonebot import get_driver
 from nonebot.log import logger
 from playwright.async_api import Browser, async_playwright
+
+from . import config
 
 _browser: Optional[Browser] = None
 
@@ -45,10 +50,31 @@ async def get_dynamic_screenshot(url):
 
 
 def install():
+    """自动安装、更新 Chromium"""
+
     logger.info("正在检查 Chromium 更新")
     import sys
     from playwright.__main__ import main
     sys.argv = ['', 'install', 'chromium']
     main()
 
+
+def delete_pyppeteer():
+    """删除 Pyppeteer 遗留的 Chromium"""
+
+    dir = Path(AppDirs('pyppeteer').user_data_dir)
+    if not dir.exists():
+        return
+
+    if not config.haruka_delete_pyppeteer:
+        logger.info("检测到 Pyppeteer 依赖（约 300 M)，"
+                    "新版 HarukaBot 已经不需要这些文件了。"
+                    "如果没有其他程序依赖 Pyppeteer，请在 '.env.*' 中设置"
+                    " 'HARUKA_DELETE_PYPPETEER=True' 并重启 Bot 后，将自动清除残留")
+    else:
+        shutil.rmtree(dir)
+        logger.info("已清理 Pyppeteer 依赖残留")
+
+
+get_driver().on_startup(delete_pyppeteer)
 get_driver().on_startup(install)
