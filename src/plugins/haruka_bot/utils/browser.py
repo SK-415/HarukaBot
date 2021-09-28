13110f180ccee5d7ca62b1c6c4147831fc6124fa
SK-415 <2967923486@qq.com>
2021-09-27 22:47:40 -0400

    increase dpi of screenshot

--- src/plugins/haruka_bot/utils/browser.py
+++ src/plugins/haruka_bot/utils/browser.py
@@ -28,19 +28,19 @@ async def get_dynamic_screenshot(url):
     browser = await get_browser()
     page = None
     try:
-        page = await browser.new_page()
+        page = await browser.new_page(device_scale_factor=2)
         await page.goto(url, wait_until='networkidle', timeout=10000)
         await page.set_viewport_size({"width": 2560, "height": 1080})
         card = await page.query_selector(".card")
-        assert card is not None
+        assert card
         clip = await card.bounding_box()
-        assert clip is not None
+        assert clip
         bar = await page.query_selector(".text-bar")
-        assert bar is not None
+        assert bar
         bar_bound = await bar.bounding_box()
-        assert bar_bound is not None
+        assert bar_bound
         clip['height'] = bar_bound['y'] - clip['y']
-        image = await page.screenshot(clip=clip)
+        image = await page.screenshot(clip=clip, full_page=True)
         await page.close()
         return base64.b64encode(image).decode()
     except Exception:
