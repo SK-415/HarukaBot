b5fef024f1be62332891008daa043598717692ef
SK-415 <2967923486@qq.com>
2021-09-27 21:48:30 -0400

    update dependencies

--- bot.py
+++ bot.py
@@ -1,31 +1,32 @@
-from os import path
-
 import nonebot
 from nonebot.adapters.cqhttp import Bot
 from nonebot.log import default_format, logger
 
 
+logger.add(
+    "logs/error.log",
+    rotation="00:00",
+    retention="1 week",
+    diagnose=False,
+    level="ERROR",
+    format=default_format,
+    encoding="utf-8",
+)
+
 nonebot.init()
-driver = nonebot.get_driver()
-driver.register_adapter('cqhttp', Bot)
 app = nonebot.get_asgi()
 
-nonebot.load_plugins("src/plugins")
+driver = nonebot.get_driver()
+driver.register_adapter("cqhttp", Bot)
+
+nonebot.load_from_toml("pyproject.toml")
 
 # Modify some config / config depends on loaded configs
-# 
-# config = nonebot.get_driver().config
+#
+# config = driver.config
 # do something...
 
-logger.add(path.join('log', "error.log"),
-           rotation="00:00",
-           retention='1 week',
-           diagnose=False,
-           level="ERROR",
-           format=default_format,
-           encoding='utf-8'
-           )
-           
 
 if __name__ == "__main__":
-    nonebot.run(app="bot:app")
+    nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
+    nonebot.run(app="__mp_main__:app")
