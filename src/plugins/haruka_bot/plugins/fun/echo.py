f3c673a38a96455a8040ae4ce370b684f6772af6
k1ee <cnschwarzer@qq.com>
2021-11-01 20:10:00 +0800

    Weibo

--- src/plugins/haruka_bot/plugins/fun/echo.py
+++ src/plugins/haruka_bot/plugins/fun/echo.py
@@ -1,7 +1,8 @@
 from nonebot import on_command
 from nonebot.adapters.cqhttp import Bot
-from nonebot.adapters.cqhttp.event import MessageEvent, MessageSegment
+from nonebot.adapters.cqhttp import MessageEvent, MessageSegment
 from nonebot.typing import T_State
+from nonebot.log import logger
 
 from ...database import DB
 from ...utils import get_type_id, permission_check, to_me, handle_uid
@@ -10,8 +11,8 @@ echo = on_command('还能说话吗', rule=to_me(), priority=5)
 
 @echo.handle()
 async def _(bot: Bot, event: MessageEvent, state: T_State):
+    logger.info(event)
+    logger.info(event.user_id)
     message = MessageSegment.at(event.user_id) + "还能说话吗"
-    print(event)
-    print(event.user_id)
-    print(message)
+    logger.info(message)
     await echo.finish(message)
