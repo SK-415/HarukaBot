e4438a79b77719be2c4c9d1dfc243a765c9bb0ce
k1ee <cnschwarzer@qq.com>
2021-11-01 19:57:24 +0800

    Fun

--- src/plugins/haruka_bot/plugins/fun/echo.py
+++ src/plugins/haruka_bot/plugins/fun/echo.py
@@ -7,9 +7,9 @@ from ...database import DB
 from ...utils import get_type_id, permission_check, to_me, handle_uid
 
 echo = on_command('还能说话吗', rule=to_me(), priority=5)
-echo.__doc__ = """还能说话吗"""
+echo.__doc__ = None
 
-
+@echo.handle()
 async def _(bot: Bot, event: MessageEvent, state: T_State):
     message = MessageSegment.at(event.user_id) + "还能说话吗"
     await echo.finish(message)
