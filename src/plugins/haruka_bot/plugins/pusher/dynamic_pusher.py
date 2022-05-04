b9c99c3a2c8dae27e06fa384c24933549d6be81b
djkcyl <cyl@cyllive.cn>
2022-05-04 21:17:10 +0800

    v1.4.1 修改动态推送 api 为 gRPC 接口 推送效率+++++++++++++

--- src/plugins/haruka_bot/plugins/pusher/dynamic_pusher.py
+++ src/plugins/haruka_bot/plugins/pusher/dynamic_pusher.py
@@ -1,19 +1,19 @@
 import asyncio
 import traceback
-from datetime import datetime, timedelta
 
-from bilireq.dynamic import get_user_dynamics
 from nonebot.log import logger
+from nonebot.adapters.onebot.v11.message import MessageSegment
 
 from ... import config
 from ...database import DB as db
-from ...libs.dynamic import Dynamic
-from ...utils import PROXIES, get_dynamic_screenshot, safe_send, scheduler
+from ...libs.grpc import grpc_dyn_get
+from ...libs.grpc.bilibili.app.dynamic.v2.dynamic_pb2 import DynamicType
+from ...utils import get_dynamic_screenshot, safe_send, scheduler
 
-last_time = {}
+OFFSET = {}
 
 
-@scheduler.scheduled_job("interval", seconds=config.haruka_interval, id="dynamic_sched")
+@scheduler.scheduled_job("interval", seconds=1, id="dynamic_sched")
 async def dy_sched():
     """直播推送"""
 
@@ -24,33 +24,33 @@ async def dy_sched():
     assert user is not None
     name = user.name
 
+    # 获取 up 个人主页
+    user_dyn_page = await grpc_dyn_get(uid)
+    dynamics = user_dyn_page.list
+    if dynamics:
+        # 如果 up 动态页正常则更新 up 名字
+        name = dynamics[0].modules[0].module_author.author.name
+
     logger.debug(f"爬取动态 {name}（{uid}）")
-    # 获取最近十二条动态
-    dynamics = (await get_user_dynamics(uid, proxies=PROXIES)).get("cards", [])
+
     # config['uid'][uid]['name'] = dynamics[0]['desc']['user_profile']['info']['uname']
     # await update_config(config)
 
-    if len(dynamics) == 0:  # 没有发过动态或者动态全删的直接结束
-        return
-
-    if uid not in last_time:  # 没有爬取过这位主播就把最新一条动态时间为 last_time
-        dynamic = Dynamic(**dynamics[0])
-        last_time[uid] = dynamic.time
+    if uid not in OFFSET:  # 没有爬取过这位主播就把最新一条动态时间为 last_time
+        dynamic = dynamics[0]
+        OFFSET[uid] = int(dynamic.extend.dyn_id_str)
         return
 
     dynamic = None
     for dynamic in dynamics[::-1]:  # 从旧到新取最近5条动态
-        dynamic = Dynamic(**dynamic)
-        if (
-            dynamic.time > last_time[uid]
-            and dynamic.time
-            > datetime.now().timestamp() - timedelta(minutes=10).seconds
-        ):
-            logger.info(f"检测到新动态（{dynamic.id}）：{name}（{uid}）")
+        dyn_id = int(dynamic.extend.dyn_id_str)
+        if dyn_id > OFFSET[uid]:
+            logger.info(f"检测到新动态（{dyn_id}）：{name}（{uid}）")
+            url = f"https://t.bilibili.com/{dyn_id}"
             image = None
             for _ in range(3):
                 try:
-                    image = await get_dynamic_screenshot(dynamic.url)
+                    image = await get_dynamic_screenshot(url)
                     break
                 except Exception:
                     logger.error("截图失败，以下为错误日志:")
@@ -58,7 +58,22 @@ async def dy_sched():
                 await asyncio.sleep(0.1)
             if not image:
                 logger.error("已达到重试上限，将在下个轮询中重新尝试")
-            await dynamic.format(image)
+
+            type_msg = {
+                0: "发布了新动态",
+                DynamicType.forward: "转发了一条动态",
+                DynamicType.word: "发布了新文字动态",
+                DynamicType.draw: "发布了新图文动态",
+                DynamicType.av: "发布了新投稿",
+                DynamicType.article: "发布了新专栏",
+                DynamicType.music: "发布了新音频",
+            }
+            message = (
+                f"{name} "
+                + f"{type_msg.get(dynamic.card_type, type_msg[0])}：\n"
+                + f"{url}\n"
+                + MessageSegment.image(f"base64://{image}")
+            )
 
             push_list = await db.get_push_list(uid, "dynamic")
             for sets in push_list:
@@ -66,11 +81,11 @@ async def dy_sched():
                     bot_id=sets.bot_id,
                     send_type=sets.type,
                     type_id=sets.type_id,
-                    message=dynamic.message,
+                    message=message,
                     at=bool(sets.at) and config.haruka_dynamic_at,
                 )
 
-            last_time[uid] = dynamic.time
+            OFFSET[uid] = dyn_id
 
     if dynamic:
-        await db.update_user(uid, dynamic.name)
+        await db.update_user(uid, name)
