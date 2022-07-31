0ab44da4a12077e53af1cdb41751a735c3e4e451
SK <36433929+SK-415@users.noreply.github.com>
2022-07-31 02:19:25 +0000

    ♻️ improve group only command

--- src/plugins/haruka_bot/plugins/permission/permission_on.py
+++ src/plugins/haruka_bot/plugins/permission/permission_on.py
@@ -1,12 +1,10 @@
-from typing import Union
 from nonebot import on_command
-from nonebot.adapters.onebot.v11.event import GroupMessageEvent, PrivateMessageEvent
-from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent
 from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
+from nonebot.permission import SUPERUSER
 
 from ...database import DB as db
-from ...utils import to_me
-
+from ...utils import group_only, to_me
 
 permission_on = on_command(
     "开启权限",
@@ -16,14 +14,12 @@ permission_on = on_command(
 )
 permission_on.__doc__ = """开启权限"""
 
+permission_on.handle()(group_only)
+
 
 @permission_on.handle()
-async def _(event: Union[PrivateMessageEvent, GroupMessageEvent]):
+async def _(event: GroupMessageEvent):
     """开启当前群权限"""
-
-    if isinstance(event, PrivateMessageEvent):
-        await permission_on.finish("只有群里才能开启权限")
-        return  # IDE 快乐行
     if await db.set_permission(event.group_id, True):
         await permission_on.finish("已开启权限，只有管理员和主人可以操作")
     await permission_on.finish("权限已经开启了，只有管理员和主人可以操作")
