df9c673b9b6e7b309bd24d9ca09f1699bc34460d
SK <36433929+SK-415@users.noreply.github.com>
2022-08-02 05:52:43 +0000

    ✨ feat: add validator to interval configs

--- src/plugins/haruka_bot/config.py
+++ src/plugins/haruka_bot/config.py
@@ -1,11 +1,10 @@
 from typing import Optional
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 
 # 其他地方出现的类似 from .. import config，均是从 __init__.py 导入的 Config 实例
 class Config(BaseSettings):
-
     fastapi_reload: bool = False
     haruka_dir: Optional[str] = None
     haruka_to_me: bool = True
@@ -15,6 +14,13 @@ class Config(BaseSettings):
     haruka_live_interval: int = haruka_interval
     haruka_dynamic_interval: int = 1
     haruka_dynamic_at: bool = False
+    haruka_screenshot_style: str = "mobile"
+
+    @validator("haruka_interval", "haruka_live_interval", "haruka_dynamic_interval")
+    def positive_only(cls, v):
+        if v < 1:
+            return 1
+        return v
 
     class Config:
         extra = "ignore"
